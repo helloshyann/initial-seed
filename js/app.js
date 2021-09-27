8f0897c6f9b28d6e8e1fa0c4db63097322e6368d
helloshyann <helloshyann@gmail.com>
2021-09-27 09:23:26 -0700

    Update Seed

---
+++ js/app.js
@@ -0,0 +1,24 @@
+
+
+const fetchPokemon = () => {
+
+    const promises = [];
+    
+    for (let i = 1; 1 <= 10; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json())); 
+    }
+
+    Promise.all(promises).then(results => {
+        const pokemon = results.map((data) => ({
+            name: data.name,
+            id: data.id,
+            image: data.sprites['front_default'],
+            type: data.types.map((type) => type.type.name).join(", ")
+        }));
+        console.log(pokemon);
+    })   
+    
+};
+
+fetchPokemon();
